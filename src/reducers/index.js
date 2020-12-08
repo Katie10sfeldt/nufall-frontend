8bf7b842f24597094aa42101fd0e8a9f3982d28f
Katie Tensfeldt <kmtensfeldt@gmail.com>
2020-12-08 10:04:10 -0800

    Build action creators for all types

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,10 @@
 import { combineReducers } from 'redux';
+import { reducer as formReducer } from 'redux-form';
 import authReducer from './authReducer';
 
 export default combineReducers({
 	auth: authReducer,
+	form: formReducer,
 });
+
+//the form reducer is coming from the redux-form package
