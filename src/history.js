61d4dcf5be100680221ed1e5863e420657a233f3
Katie Tensfeldt <kmtensfeldt@gmail.com>
2020-12-09 11:41:30 -0800

    Set up basic functionality

---
+++ src/history.js
@@ -0,0 +1,5 @@
+//this creates a new history object that we can access - allows us to direct a user as we see fit
+
+import { createBrowserHistory } from 'history';
+
+export default createBrowserHistory();
